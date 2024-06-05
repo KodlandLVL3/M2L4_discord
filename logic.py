6d3b198c93b86375851a0b606a165298d7ca77d0
Kootyra <110038102+Kootyra@users.noreply.github.com>
2024-06-05 16:48:52 +0200

    first commit

--- logic.py
+++ logic.py
@@ -1,11 +1,10 @@
 from translate import Translator
-import requests
 from collections import defaultdict
 
-qwestions = {'Как тебя зовут' : "Я супер-крутой-бот и мое предназначение помогать тебе!",
-             "Сколько тебе лет" : "Это слишком философский вопрос"}
+questions = {'Как тебя зовут': "Я супер-крутой-бот и мое предназначение помогать тебе!",
+             "Сколько тебе лет": "Это слишком философский вопрос"}
 
-class TextAnalysis():   
+class TextAnalysis():
     
     memory = defaultdict(list)
 
@@ -16,30 +15,20 @@ class TextAnalysis():
         self.text = text
         self.translation = self.__translate(self.text, "ru", "en")
 
-        if self.text in qwestions.keys():
-            self.response = qwestions[self.text]
+        if self.text in questions.keys():
+            self.response = questions[self.text]
         else:
             self.response = self.get_answer() 
 
     
     def get_answer(self):
-        res = self.__translate(self.__deep_pavlov_answer(), "en", "ru")
+        res = self.__translate("I don't know how to help", "en", "ru")
         return res
 
     def __translate(self, text, from_lang, to_lang):
         try:
-            translator= Translator(from_lang=from_lang, to_lang=to_lang)
+            translator = Translator(from_lang=from_lang, to_lang=to_lang)
             translation = translator.translate(text)
             return translation
         except:
             return "Перевод не удался"
-
-    def __deep_pavlov_answer(self):
-        try:
-            API_URL = "https://7038.deeppavlov.ai/model"
-            data = {"question_raw": [ self.translation ]}
-            res = requests.post(API_URL, json=data).json()
-            res = res[0][0]
-        except:
-            res = "I don't know how to help"
-        return res
